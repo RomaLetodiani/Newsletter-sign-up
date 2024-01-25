d52f6937f06099eb4caac5d827c99fc80e52d755
RomaLetodiani <roman.letodiani03@gmail.com>
2024-01-25 16:49:58 +0400

    added consts

---
+++ src/components/shared/consts.ts
@@ -0,0 +1,51 @@
+// <!-- Sign-up form start -->
+
+// Stay updated!
+
+// Join 60,000+ product managers receiving monthly updates on:
+
+// Product discovery and building what matters
+// Measuring to ensure updates are a success
+// And much more!
+
+// Email address
+// email@company.com
+
+// Subscribe to monthly newsletter
+
+// <!-- Sign-up form end -->
+
+// <!-- Success message start -->
+
+// Thanks for subscribing!
+
+// A confirmation email has been sent to ash@loremcompany.com.
+// Please open it and click the button inside to confirm your subscription.
+
+// Dismiss message
+
+// <!-- Success message end -->
+
+const textObj = {
+  signUp: {
+    title: 'Stay updated!',
+    desc: 'Join 60,000+ product managers receiving monthly updates on:',
+    list: [
+      'Product discovery and building what matters',
+      'Measuring to ensure updates are a success',
+      'And much more!',
+    ],
+    input: {
+      title: 'Email address',
+      placeholder: 'email@company.com',
+    },
+    button: 'Subscribe to monthly newsletter',
+  },
+  success: {
+    title: 'Thanks for subscribing!',
+    desc1: 'A confirmation email has been sent to',
+    desc2:
+      'Please open it and click the button inside to confirm your subscription.',
+    button: 'Dismiss message',
+  },
+};
