667cf13862093f7574697a4f3239cd2769917178
RomaLetodiani <roman.letodiani03@gmail.com>
2024-01-27 02:38:05 +0400

    built input component

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -11,14 +11,25 @@ type Props = {
 
 const Input = (props: Props) => {
   return (
-    <div className="w-full">
+    <div className="w-full mt-5">
       <div className="flex justify-between w-full">
-        <label htmlFor={props.id}>{props.label}</label>
-        {props.error && <p className="text-tomato">{props.errorMessage}</p>}
+        <label
+          className="text-darkSlateGray font-bold text-sm"
+          htmlFor={props.id}
+        >
+          {props.label}
+        </label>
+        {props.error && (
+          <p className="text-tomato text-sm font-bold">{props.errorMessage}</p>
+        )}
       </div>
       <input
         placeholder={props.placeholder}
-        className={`w-full border-b border-black`}
+        className={`w-full outline-none mt-2 border  rounded-lg px-5 py-4 text-darkSlateGray ${
+          props.error
+            ? 'border-tomato bg-tomatoLight text-tomato placeholder:text-tomato'
+            : 'border-grey'
+        }`}
         value={props.value}
         onChange={props.onChange}
         onBlur={props.onBlur}
