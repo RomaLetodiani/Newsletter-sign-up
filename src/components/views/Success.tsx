501813f9c33107aa0e7ade1d59ba4f38f1021f3c
RomaLetodiani <roman.letodiani03@gmail.com>
2024-01-27 02:38:58 +0400

    built Success Page

--- src/components/views/Success.tsx
+++ src/components/views/Success.tsx
@@ -1,3 +1,7 @@
+import Button from '../UI/Button';
+import { textObj } from '../shared/consts';
+import { SuccessIcon } from '../shared/icons';
+
 const Success = ({
   setView,
   email,
@@ -5,11 +9,18 @@ const Success = ({
   setView: () => void;
   email: string | null;
 }) => {
+  const { success } = textObj;
   return (
-    <div>
-      {email}
-      <br />
-      <button onClick={setView}>suc</button>
+    <div className="flex flex-col justify-center h-full max-w-md p-8">
+      <SuccessIcon />
+      <h1 className="text-4xl min-[450px]:text-5xl my-7 font-bold text-darkSlateGray ">
+        {success.title}
+      </h1>
+      <p className="text-darkSlateGray">
+        {success.desc1} <span className="font-bold">{email}</span>{' '}
+        {success.desc2}
+      </p>
+      <Button text={success.button} onClick={setView} />
     </div>
   );
 };
